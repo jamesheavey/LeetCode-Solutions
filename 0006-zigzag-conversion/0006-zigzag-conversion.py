597cc6bcfdc8fdd7a53358d1502e12c266db6197
James Heavey <76979225+jamesheavey@users.noreply.github.com>
2022-10-18 00:57:39 +0100

    Time: 3442 ms (5.01%), Space: 14.1 MB (49.26%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,28 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+            
+        zz = [[] for i in range(numRows)]
+        row = col = -1
+        s += ''
+        while s != '':
+            
+            row += 1
+            row = row % numRows
+            if row == 0:
+                col += 1
+            
+            if col % (numRows-1) == 0:
+                zz[row].append(s[0])
+                s = s[1:]
+            elif row == numRows-1-(col%(numRows-1)):
+                zz[row].append(s[0])
+                s = s[1:]
+                
+        zz_string = ''
+        for x in zz:
+            zz_string += ''.join(x)
+            
+        return zz_string
+            
