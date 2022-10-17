8000714c75be346aae91750944ada938f1d9cbd6
James Heavey <76979225+jamesheavey@users.noreply.github.com>
2022-10-18 00:58:51 +0100

    Time: 1597 ms (5.36%), Space: 14.2 MB (13.98%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -4,14 +4,9 @@ class Solution:
             return s
             
         zz = [[] for i in range(numRows)]
-        row = col = -1
+        row = col = 0
         
         while s != '':
-            
-            row += 1
-            row = row % numRows
-            if row == 0:
-                col += 1
             
             if col % (numRows-1) == 0:
                 zz[row].append(s[0])
@@ -19,6 +14,11 @@ class Solution:
             elif row == numRows-1-(col%(numRows-1)):
                 zz[row].append(s[0])
                 s = s[1:]
+                
+            row += 1
+            row = row % numRows
+            if row == 0:
+                col += 1
                 
         zz_string = ''
         for x in zz:
